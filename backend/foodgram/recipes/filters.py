609a6e233e858951811a45b531822535b1f73274
Alex Leontiev <alexeont@yandex.ru>
2024-02-09 03:14:54 +0300

    fixed_cart

--- backend/foodgram/recipes/filters.py
+++ backend/foodgram/recipes/filters.py
@@ -3,12 +3,23 @@ from django_filters import FilterSet, filters
 from .models import Recipe
 
 
+BOOLEAN_CHOICES = (
+    (0, False),
+    (1, True)
+)
+
+
 class RecipeFilter(FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='filter_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='filter_shopping_cart')
+    is_favorited = filters.ChoiceFilter(choices=BOOLEAN_CHOICES,
+                                        method='filter_favorited')
+    is_in_shopping_cart = filters.ChoiceFilter(choices=BOOLEAN_CHOICES,
+                                               method='filter_shopping_cart')
 
     # Изменил на Boolean, теперь воспринимает только значения True/False.
+
+    # upd: изменил обратно, потому что без этого фронт
+    # не корректно выводил рецепты
 
     class Meta:
         model = Recipe
@@ -19,12 +30,12 @@ class RecipeFilter(FilterSet):
 
     def filter_favorited(self, queryset, name, value):
         user = self.request.user
-        if value:
+        if value == '1':
             return queryset.filter(favorites__consumer=user)
         return queryset
 
     def filter_shopping_cart(self, queryset, name, value):
         user = self.request.user
-        if value:
+        if value == '1':
             return queryset.filter(cart_items__consumer=user)
         return queryset
